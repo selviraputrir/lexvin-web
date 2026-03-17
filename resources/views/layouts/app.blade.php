15b35148d6343582b7f157eaca983d0d512e476c
selvira putri <selviraputrir05@gmail.com>
2026-03-17 23:13:15 +0700

    Menambahkan meta tag verifikasi Google

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="id" class="scroll-smooth">
 <head>
+    <meta name="google-site-verification" content="RKpHBGEEfHIBM0ZgetWez8rYNp4vgzbdr-KDCit7t0U" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LEXVIN</title>
