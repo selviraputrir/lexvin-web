62cdadf0dbd7b2bb5babe668375ddc4dc269f043
selvira putri <selviraputrir05@gmail.com>
2026-03-19 14:48:56 +0700

    upload app and contact

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta name="google-site-verification" content="RKpHBGEEfHIBM0ZgetWez8rYNp4vgzbdr-KDCit7t0U" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LEXVIN</title>
+    <title>LEXVINLaw</title>
     <link rel="icon" href="{{ asset('images/logo-lexvinlaw.jpeg') }}" type="image/jpeg">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LEXVIN</title>
+    <title>LEXVINLAW</title>
      <link rel="icon" href="{{ asset('images/logo-lexvinlaw.jpeg') }}" type="image/jpeg">
     
     <script src="https://cdn.tailwindcss.com"></script>
